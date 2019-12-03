211d438dc72587b4b4ddfe248f11918f49ac1ae7
James Wright <james@jamesswright.co.uk>
2019-12-03 11:39:17 +0000

    WIP attempt of shuffling input data

--- encryption/singh.py
+++ encryption/singh.py
@@ -1,2 +1,19 @@
-def generateKeyPair():
-  []
+magic = [13, 77, 92, 108, 119, 181, 203, 261]
+magic_len = len(magic)
+
+def swap(data, i, j):
+  data[j] = data[i] + data[j]
+  data[i] = data[j] - data[i]
+  data[j] = data[j] - data[i]
+
+def shuffle(data):
+  x = 1
+  shuffled = list.copy(data)
+  data_len = len(data)
+
+  for j in range(1, magic_len + 1):
+    for k in range(1, data_len + 1):
+      x = ((j + 1) * x + magic[(j + k) % magic_len]) % data_len
+      swap(shuffled, k, x)
+
+  shuffled
